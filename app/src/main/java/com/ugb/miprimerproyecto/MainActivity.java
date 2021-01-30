98f9b7a81a9a36cefaa73382b815bd7928b1f932
Jose Roberto <robertodelrio.1000@gmail.com>
2021-01-30 17:50:36 -0600

    calculadora basica completa

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -3,8 +3,10 @@ package com.ugb.miprimerproyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -14,58 +16,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calcular(View view){
-        TextView tempVal =(TextView)findViewById(R.id.txtnum1);
+
+    public void calcular(View view) {
+        TextView tempVal = (TextView) findViewById(R.id.txtnum1);
         double num1 = Double.parseDouble(tempVal.getText().toString());
 
-        tempVal = (TextView)findViewById(R.id.txtnum2);
-        double num2 =Double.parseDouble(tempVal.getText().toString());
+        tempVal = (TextView) findViewById(R.id.txtnum2);
+        double num2 = Double.parseDouble(tempVal.getText().toString());
 
         double respuesta = 1;
-        RadioButton optOperacionesBasicas = findViewById(R.id.optsuma);
-        if(optOperacionesBasicas.isChecked()) {
-                respuesta = num1 + num2;
-            }
-            optOperacionesBasicas = findViewById(R.id.optResta);
-            if(optOperacionesBasicas.isChecked()) {
-                respuesta = num1 - num2;
-            }
-
-                optOperacionesBasicas = findViewById(R.id.optMultiplicacion);
-                if(optOperacionesBasicas.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-
-                    optOperacionesBasicas = findViewById(R.id.optDivision);
-                    if(optOperacionesBasicas.isChecked()) {
-                        respuesta = num1 / num2;
-                    }
-        RadioButton optOperacionesBasicas = findViewById(R.id.optfactorial);
-          // i++ -> i = i + 1 -> i+=1
-        if(optOperacionesBasicas.isChecked()) {
-            for(int i=1; i<=num1; i++)
-                respuesta *=i;
-               }
+        RadioButton optOperacionesBasicas = findViewById(R.id.optSuma);
+        if (optOperacionesBasicas.isChecked()) {
+            respuesta = num1 + num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optResta);
+        if (optOperacionesBasicas.isChecked()) {
+            respuesta = num1 - num2;
         }
 
-        Spinner cboOperacionesBasicas = findViewById(R.id.cboOperacionesBasicas);
-        Switch (cboOperacionesBasicas.getSelectItemPosition()){
-        case 0:
-            respuesta = num1+num2;
-            break;
-        case 1:
-            respuesta = num1-num2;
-            break;
-        case 2:
-            respuesta = num1*num2;
-            break;
-        case 3:
-            respuesta = num/num2;
+        optOperacionesBasicas = findViewById(R.id.optMultiplicacion);
+        if (optOperacionesBasicas.isChecked()) {
+            respuesta = num1 * num2;
+        }
 
+        optOperacionesBasicas = findViewById(R.id.optDivision);
+        if (optOperacionesBasicas.isChecked()) {
+            respuesta = num1 / num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optfactorial);
+        // i++ -> i = i + 1 -> i+=1
+        if (optOperacionesBasicas.isChecked()) {
+            for (int i = 1; i <= num1; i++) {
+                respuesta *= i;
+            }
+        }
+        optOperacionesBasicas = findViewById(R.id.optPorcentaje);
+        if (optOperacionesBasicas.isChecked()) {
+                respuesta = ((num1 / num2) * 100);
+            }
 
-
+        optOperacionesBasicas = findViewById(R.id.optexponenciacion);
+        if (optOperacionesBasicas.isChecked()) {
+                    double acumulador = num1;
+                    for (int i =1; i <=num2; i++);{
+                        respuesta = acumulador * respuesta;
+                    }
+                }
+        optOperacionesBasicas = findViewById(R.id.optRaiz);
+        if (optOperacionesBasicas.isChecked()) {
+            double RaizN = Math.pow(num2, 1/num1);
+            respuesta = RaizN;
+        }
+         optOperacionesBasicas = findViewById(R.id.optModulo);
+        if (optOperacionesBasicas.isChecked()) {
+            respuesta = num1 % num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optMayorde2numeros);
+        if (optOperacionesBasicas.isChecked()) {
+            if (num1 > num2){
+                respuesta = num1;
+            }
+            if (num2 > num1){
+                respuesta = num2;
+            }
+        }
 
         tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta"+ respuesta);
-    }
-}
+        tempVal.setText("Respuesta" + respuesta);
+
+            }
+        }
+
